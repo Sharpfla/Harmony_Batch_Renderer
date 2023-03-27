391eb0c147372ea3b64a9281e3b6e5b208c0dc8f
SharpFla <electricwalker@gmail.com>
2023-03-27 17:03:39 -0500

    readme

--- Readme.md
+++ Readme.md
@@ -1,10 +1,17 @@
 This tool renders multiple harmony file by searching a directory for xstage files following its naming convention
 the newest version of a shot is determined by this naming convention
 
-The file naming convention is A1_POS_JDoe
+SETUP
+
+1. The file naming convention is A1_POS_JDoe
 - The shot numbers are listed A01-Z99
 - Rendering for newest file order is CMP > PNT > CLN > RUF > POS
 
-first input the target directory
+2. Enter api\config.py and adjust the settings
+    - Set the SEARCH_DIR to the location of your harmony files
+    - Change INSTALL_DIR to the location of batch renderer's repository
+    - Change SELECTED_HARMONY_VERSION to the version installed on you machine
 
-that's it
+3. Run main.py
+    - Say no to changing search directory (will be better implemented in later versions)
+    - Enter the same harmony version as you entered in config.py
--- api/config.py
+++ api/config.py
@@ -1,11 +1,11 @@
 import os
 class config:
-    SEARCH_DIR=r"C:\Users\391524\Documents\test_shots\test_shots"
+    SEARCH_DIR=r"E:\root\2022.anim.proj.APSscreamwriters\shots\CMP\C_SEQ"
     # TOON_BOOM_PATH = r'C:\Program Files (x86)\Toon Boom Animation\Toon Boom Harmony 21 Premium\win64\bin'
 
     # You can update these paths to include support for new versions
     VERBOSE = False
-    INSTALL_DIR = r"C:\Users\391524\Harmony_Batch_Renderer"
+    INSTALL_DIR = r"C:\Users\Sharp\Harmony_Batch_Renderer"
     # CONFIG_SCRIPT = os.path.join(INSTALL_DIR,"/api/scripts/renderconfig.js")
 
     supported_harmony_exes = {
@@ -14,7 +14,7 @@ class config:
         "V21.1": r"C:\Program Files (x86)\Toon Boom Animation\Toon Boom Harmony 21.1 Premium",
         "V22": r"C:\Program Files (x86)\Toon Boom Animation\Toon Boom Harmony 22 Premium"
     }
-    SELECTED_HARMONY_VERSION = "V21"
+    SELECTED_HARMONY_VERSION = "V22"
     HARMONY_EXE = os.path.join(supported_harmony_exes[SELECTED_HARMONY_VERSION], \
         r"win64\bin\HarmonyPremium.exe")
 
