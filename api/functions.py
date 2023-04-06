7ba7359a6d3d659ae02ba0ea3bba84d60f195c84
Will Walker <willwalker@Wills-MacBook-Pro.local>
2023-04-06 11:24:02 -0500

    better

--- api/functions.py
+++ api/functions.py
@@ -2,76 +2,67 @@ import os, glob
 import subprocess
 import logging
 import datetime
-from api.createskiplist import skip
-
-
+# from api.createskiplist import skip
 
 logging.basicConfig(filename='all_rendered_shots.txt', level=logging.DEBUG, filemode='a', format='%(message)s %(asctime)s')
 
-# get the current date and time
+class skip:
+    # open the text file in read mode
+    with open('skiplist.txt', 'r') as file:
+        # read the contents of the file
+        contents = file.read()
+
+        # split the contents of the file into a list of strings, using a comma as the delimiter
+        skiplist = contents.split(',')
+
+        # convert the list of strings to a list of integers
+        skiplist = [str(x) for x in skiplist]
+
+    # print the list of values
+    print(skiplist)
+
+# get the current date and time'
+def shot_name2num(filename):
+    filename = os.path.basename(filename) 
+    raw_ver_num = filename.split('_')[0]
+    aidx = (ord('a') - ord(raw_ver_num[0].lower())) * 100
+    true_ver_num = aidx + int(raw_ver_num[1:])
+    return true_ver_num
+
 now = datetime.datetime.now()
-
-
-
-
-
-def find_xstage_files(dir, sort=False):
-    names = glob.glob(os.path.join(dir,"**\\*.xstage"), recursive=True)
-    return names
-
-def group_xstage_files(dir):
-    #A01_CMP_JDoe  {idx}{typ}{shot_name}.xstage
-    shots = dict()
-    for fname in find_xstage_files(dir):
-        data = os.path.basename(fname).split("_")
-        extension = data[-1].split(".")[-1] if "." in data[-1] else None
-        snum = data[0]
-        print(data)
-        if len(data) >= 2: # added check for length of data
-            typ = data[1]
-        else:
-            typ = None
-
-
-
-        name = "_".join(data[2:]).strip()
-
-        if name not in shots:
-            shots[name] = dict()
-        if snum not in shots[name]:
-            shots[name][snum] = dict()
-        if typ != None: # only add to dictionary if typ is not None
-            shots[name][snum][typ.upper()] = fname
-    return shots
-
 def render_latests(cfg):
-    jobs = group_xstage_files(cfg.SEARCH_DIR)
-    for jname, jdata in jobs.items():
-        for snum, version_data in jdata.items():
-            typ = None
-            # Search for .xstage files containing "CMP" in the filename and write their paths to the output file
-            if "CMP" in version_data:
-                typ = "CMP"
-                fname = os.path.abspath(version_data['CMP'])
-            elif "PNT" in version_data:
-                typ = "PNT"
-                fname = os.path.abspath(version_data['PNT'])
-            elif "CLN" in version_data:
-                typ = "CLN"
-                fname = os.path.abspath(version_data['CLN'])
-            if snum in skip.skiplist:
-                typ = None
-            if typ != None:
-                print(snum, typ, jname)
-                completed = subprocess.run([cfg.HARMONY_EXE,"-batch",fname], shell=True, capture_output=True)
-                logging.debug(f'{snum}, {typ}, {jname}')
-                # print(completed)
+    test_shots = search_for_shots(cfg.SEARCH_DIR)
+    for shot in sorted(test_shots, key=shot_name2num):
+        if shot in skip.skiplist:
+            continue
 
+        latest_version = min(test_shots[shot], key=lambda ext: cfg.RENDER_ORDER.get(ext.upper(), 1000))
+        print(f"Rendering {test_shots[shot][latest_version]}")
+        # completed = subprocess.run([cfg.HARMONY_EXE,"-batch",fname], shell=True, capture_output=True)
+        # logging.debug(f'{typ}, {jname}')
+        # print(completed)
+
+def search_for_shots(dir):
+    #A01_CMP_JDoe {idx}{typ}{shot_name}.xstage
+    shots = dict()
+    for fname in glob.glob(os.path.join(dir,"*.xstage"), recursive=True):
+        data = os.path.basename(fname).split("_")
+        snum = data[0]
+        typ = data[1]
+        if snum not in shots:
+            shots[snum] = dict()
+        if typ not in shots[snum]:
+            shots[snum][typ] = fname
+    return shots
 
 if __name__ == "__main__":
     # Example usage
     class Config:
         SEARCH_DIR = "C:/path/to/search/dir"
         HARMONY_EXE = "C:/path/to/harmony.exe"
+        RENDER_ORDER = {"CMP": 0, "PNT": 1, "CLN": 2, "RUF": 3, "POS": 4}
+        
     cfg = Config()
+    cfg.SEARCH_DIR = "/Users/willwalker/Harmony_Batch_Renderer/test_shots/"
+
     render_latests(cfg)
